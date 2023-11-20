ae8b777a07fe0b28627ff1aac90740b18f00a979
MasoudSh <Msh.Git.Dev@gmail.com>
2023-11-20 14:51:21 +0330

    create : How To Use Factory Pattern In Console App

---
+++ DesignPatterns.Shared.FactoryPattern/HowToUse.cs
@@ -0,0 +1,14 @@
+﻿using DesignPatterns.Shared.FactoryPattern.Factories;
+using DesignPatterns.Shared.Services;
+
+namespace DesignPatterns.Shared.FactoryPattern;
+public static class HowToUse {
+
+    public static void MakeExample() {
+        var furniture = new FurnitureFactory().Create(Consts.FurnitureType.Art);
+        furniture?.ChooseChair().TakeSeat();
+        furniture?.ChooseSofa().TakeSeat();
+        furniture?.ChooseTable().PutItem();
+        LogService.Log("Operation is Ended.");
+    }
+}
