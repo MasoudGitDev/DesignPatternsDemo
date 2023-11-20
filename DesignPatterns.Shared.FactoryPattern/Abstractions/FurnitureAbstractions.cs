7c02624e02edd01289d374c8f2fbe0e369656155
MasoudSh <Msh.Git.Dev@gmail.com>
2023-11-20 14:48:05 +0330

    create : Furniture Abstarctions

---
+++ DesignPatterns.Shared.FactoryPattern/Abstractions/FurnitureAbstractions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Shared.FactoryPattern.Abstractions; 
+
+public interface IFurnishable { }
+public interface ISeatable {
+    void TakeSeat();
+}
+public interface IPlaceable {
+    void PutItem();
+}
+
+public interface IChair : ISeatable, IFurnishable { }
+public interface ISofa : ISeatable, IFurnishable { }
+public interface ITable : IPlaceable, IFurnishable { }
+
+
+public interface IFurniture {
+    IChair ChooseChair();
+    ISofa ChooseSofa();
+    ITable ChooseTable();
+}
