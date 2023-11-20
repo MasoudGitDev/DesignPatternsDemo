0945d61de2cb860fc63c625fe9f85acab5bd2358
MasoudSh <Msh.Git.Dev@gmail.com>
2023-11-20 14:50:44 +0330

    create : Art , Classic Furniture Implementaions

---
+++ DesignPatterns.Shared.FactoryPattern/Implementations/ClassicFurniture.cs
@@ -0,0 +1,13 @@
+﻿
+using DesignPatterns.Shared.FactoryPattern.Abstractions;
+using DesignPatterns.Shared.FactoryPattern.Models.Classic;
+
+namespace DesignPatterns.Shared.FactoryPattern.Implementations;
+public class ClassicFurniture : IFurniture {
+
+    public IChair ChooseChair() => new ClassicChair();
+
+    public ISofa ChooseSofa() => new ClassicSofa();
+
+    public ITable ChooseTable() => new ClassicTable();
+}
