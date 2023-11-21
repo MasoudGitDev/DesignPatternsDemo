96f2871dc57a7fbc3ea02015a12379db96b17f81
MasoudSh <Msh.Git.Dev@gmail.com>
2023-11-21 15:53:50 +0330

    create : IHowToUse , enum DesignPatterns , HowToUseFactory

---
+++ DesignPatterns.Shared/Abstractions/IHowToUse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Shared.Abstractions {
+    public interface IHowToUse {
+        void MakeExample();
+    }
+}
