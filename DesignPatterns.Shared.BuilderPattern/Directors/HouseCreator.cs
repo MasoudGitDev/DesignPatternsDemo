fceead2b235591d692d132110e5109a6e09304ee
MasoudSh <Msh.Git.Dev@gmail.com>
2023-11-22 21:06:10 +0330

    create : House Creator and Decorator as Directors

---
+++ DesignPatterns.Shared.BuilderPattern/Directors/HouseCreator.cs
@@ -0,0 +1,28 @@
+﻿using DesignPatterns.Shared.BuilderPattern.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Shared.BuilderPattern.Directors {
+    public class HouseCreator : IHouseCreator
+    {
+        private readonly IHouseBuilder _houseBuilder;
+
+        public HouseCreator(IHouseBuilder houseBuilder)
+        {
+            _houseBuilder = houseBuilder;
+        }
+
+        public IHouseBuilder Create()
+        {
+
+            _houseBuilder.CreateFoundation();
+            _houseBuilder.CreateWalls();
+            _houseBuilder.CreateRoofs();
+
+            return _houseBuilder;
+        }
+    }
+}
---
+++ DesignPatterns.Shared.BuilderPattern/Directors/HouseDecorator.cs
@@ -0,0 +1,20 @@
+﻿using DesignPatterns.Shared.BuilderPattern.Abstractions;
+using DesignPatterns.Shared.BuilderPattern.Extensions;
+using DesignPatterns.Shared.BuilderPattern.Houses;
+
+namespace DesignPatterns.Shared.BuilderPattern.Directors {
+    public class HouseDecorator  
+    {
+        private readonly IHouseCreator _houseCreator;
+
+        public HouseDecorator(IHouseCreator houseCreator) {
+            this._houseCreator = houseCreator;
+        }
+
+        public void Decorate() {
+            var house = _houseCreator.Create();
+            house.Paint();
+            house.UseFurniture();
+        }
+    }
+}
