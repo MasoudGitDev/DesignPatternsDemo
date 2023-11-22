8f16643cdfa046ba81cc998f34f92209bb41d64a
MasoudSh <Msh.Git.Dev@gmail.com>
2023-11-22 21:07:18 +0330

    create : House Products like Stone , Normal and Luxury

---
+++ DesignPatterns.Shared.BuilderPattern/Houses/StoneHouse.cs
@@ -0,0 +1,25 @@
+﻿using DesignPatterns.Shared.BuilderPattern.Abstractions;
+using DesignPatterns.Shared.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Shared.BuilderPattern.Houses {
+    public class StoneHouse :IHouseBuilder {
+        public string HouseName { get; set; } = "Stone-House";
+
+        public void CreateFoundation() {
+            LogService.Log($"Create {HouseName} House Foundation.");
+        }
+
+        public void CreateRoofs() {
+            LogService.Log($"Create {HouseName} House Roofs.");
+        }
+
+        public void CreateWalls() {
+            LogService.Log($"Create {HouseName} walls with stones.");
+        }
+    }
+}
