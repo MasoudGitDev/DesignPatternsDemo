09549facfefcfe18b4f9fa3367fe0e10c02a3cda
MasoudSh <Msh.Git.Dev@gmail.com>
2023-11-22 21:07:50 +0330

    create : how to use for builder pattern

---
+++ DesignPatterns.Shared.BuilderPattern/HowToUse.cs
@@ -0,0 +1,32 @@
+﻿using DesignPatterns.Shared.Abstractions;
+using DesignPatterns.Shared.BuilderPattern.Directors;
+using DesignPatterns.Shared.BuilderPattern.Extensions;
+using DesignPatterns.Shared.BuilderPattern.Houses;
+using DesignPatterns.Shared.Services;
+
+namespace DesignPatterns.Shared.BuilderPattern {
+
+    public class HowToUse : IHowToUse {
+        public void MakeExample() {
+
+            // Create Normal House
+            new HouseCreator(new NormalHouse()).Create();
+
+            LogService.Log("\n");
+
+            // create Stone House with simple option:
+            new HouseCreator(new StoneHouse()).Create().Paint();
+
+            LogService.Log("\n");
+
+            // create Luxury House with full options:
+            var luxuryHouse =  new HouseCreator(new LuxuryHouse());
+            new HouseDecorator(luxuryHouse).Decorate();
+
+            
+            
+
+
+        }
+    }
+}
