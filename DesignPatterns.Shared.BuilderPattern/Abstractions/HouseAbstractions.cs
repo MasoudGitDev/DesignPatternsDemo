0e9c576ccab439c125bf569ad2076b3e8c548939
MasoudSh <Msh.Git.Dev@gmail.com>
2023-11-22 21:05:35 +0330

    create : House Abstarctions

---
+++ DesignPatterns.Shared.BuilderPattern/Abstractions/HouseAbstractions.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPatterns.Shared.BuilderPattern.Abstractions {
+
+    public interface IHouse {
+        public string HouseName { get; set; }
+    }
+    public interface IHouseCreator {
+        IHouseBuilder Create();
+    }
+    public interface IHouseBuilder :IHouse{
+        void CreateFoundation();
+        void CreateWalls();
+        void CreateRoofs();
+    }
+
+ 
+
+
+
+}
