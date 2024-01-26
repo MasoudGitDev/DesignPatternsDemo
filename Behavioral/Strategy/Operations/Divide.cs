1a30b613e7770417f530f7058d8c65220886c6de
MasoudSh <Msh.Git.Dev@gmail.com>
2024-01-26 13:55:31 +0330

    create : IMathOperation & Operations like Add,Multiply ,...

---
+++ Behavioral/Strategy/Operations/Divide.cs
@@ -0,0 +1,9 @@
+﻿using Strategy.Abstractions;
+using Strategy.Constants;
+
+namespace Strategy.Operations;
+
+internal class Divide: IMathOperation {
+    public MathOperationType MathOperationType => MathOperationType.Divide;
+    public double Calculate(double firstNumber , double secondNumber) => secondNumber is 0 ? throw new DivideByZeroException() : firstNumber / secondNumber;
+}
---
+++ Behavioral/Strategy/Operations/Subtract.cs
@@ -0,0 +1,9 @@
+﻿using Strategy.Abstractions;
+using Strategy.Constants;
+
+namespace Strategy.Operations;
+
+internal class Subtract: IMathOperation {
+    public MathOperationType MathOperationType => MathOperationType.Subtract;
+    public double Calculate(double firstNumber , double secondNumber) => firstNumber - secondNumber;
+}
