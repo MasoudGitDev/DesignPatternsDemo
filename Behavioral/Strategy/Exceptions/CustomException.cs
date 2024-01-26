d3e3b33ec70966c9877c4620f363d310e36d2f34
MasoudSh <Msh.Git.Dev@gmail.com>
2024-01-26 13:57:35 +0330

    create:MathOperatinType (enum) and Math Exceptions

---
+++ Behavioral/Strategy/Exceptions/CustomException.cs
@@ -0,0 +1,21 @@
+﻿using Strategy.Models;
+
+namespace Strategy.Exceptions;
+public class CustomException : Exception {
+    public string Where { get; } = "Unknown";
+    public string Code { get; } = "Unknown";
+    private static string newLine => Environment.NewLine;
+
+    public CustomException() {}
+    public CustomException(string message) : base(message) {}
+    public CustomException(string where , string code , string message) :
+        base($"Where : {where}{newLine}Code : {code}{newLine}Message : {message}{newLine}") {
+        Where = where;
+        Code = code;
+    }
+    public CustomException(ExceptionModel model) :
+        base($"Where : {model.Where}{newLine}Code : {model.Code}{newLine}Message : {model.Message}{newLine}") {
+        Where = model.Where;
+        Code = model.Code;
+    }
+}
---
+++ Behavioral/Strategy/Exceptions/MathOperationFactoryException.cs
@@ -0,0 +1,11 @@
+﻿namespace Strategy.Exceptions;
+public class MathOperationFactoryException : CustomException {
+    public MathOperationFactoryException() {
+    }
+
+    public MathOperationFactoryException(string message) : base(message) {
+    }
+
+    public MathOperationFactoryException(string where , string code , string message) : base(where , code , message) {
+    }
+}
