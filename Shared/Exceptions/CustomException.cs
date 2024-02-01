769b8682cca471fff94259f28de9d111ce65ea7a
MasoudSh <Msh.Git.Dev@gmail.com>
2024-02-01 21:02:45 +0330

    create : Shared Lib

---
+++ Shared/Exceptions/CustomException.cs
@@ -0,0 +1,26 @@
+﻿namespace Shared.Exceptions;
+public class CustomException :Exception {
+    public string Method { get; } = "<undefined>";
+    public string Code { get; } = "<undefined>";
+    public string Description { get; }
+    public CustomException() : base() {
+        Description = Message;
+    }
+    public CustomException(string description) : base(description) {
+        Description = description;
+    }
+    public CustomException(string code , string description)
+        :base($"Code : {code} | Description : {description}")
+    {
+        Code = code;
+        Description = description;
+    }
+    public CustomException(string method,string code , string description) 
+        : base($"Method : {method} | Code : {code} | Description : {description}") {
+        Method = method;
+        Code = code;
+        Description = description;
+    }
+
+
+}
---
+++ Shared/Extensions/ObjectCheckingExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Shared.Exceptions;
+using System.Formats.Tar;
+
+namespace Shared.Extensions;
+public static class ObjectCheckingExtensions {
+
+    public static T IfNull<T>(this T? source, string message) {
+        if(source == null)
+            throw new ArgumentNullException(message);
+        return source;
+    }
+
+    public static TObject IfNull<TException,TObject>(this TObject? source , string message) 
+        where TException : CustomException {
+        if(source == null)
+            throw (TException)Activator.CreateInstance(typeof(TException) , "NullObject" , message)!;
+        return source;
+    }
+
+
+}
