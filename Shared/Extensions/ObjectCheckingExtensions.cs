547a69e69caae56bccf1f82acb55e8a1db0309b9
MasoudSh <Msh.Git.Dev@gmail.com>
2024-02-03 17:37:28 +0330

    refactor : change extensions name

--- Shared/Extensions/ObjectCheckingExtensions.cs
+++ Shared/Extensions/ObjectCheckingExtensions.cs
@@ -3,13 +3,13 @@
 namespace Shared.Extensions;
 public static class ObjectCheckingExtensions {
 
-    public static T IfNull<T>(this T? source , string message) {
+    public static T ThrowIfNull<T>(this T? source , string message) {
         if(source == null)
             throw new CustomException("NullObject" , message);
         return source;
     }
 
-    public static TObject IfNull<TException, TObject>(this TObject? source , string message)
+    public static TObject ThrowIfNull<TException, TObject>(this TObject? source , string message)
         where TException : CustomException {
         if(source == null)
             throw (TException) Activator.CreateInstance(typeof(TException) , "NullObject" , message)!;
