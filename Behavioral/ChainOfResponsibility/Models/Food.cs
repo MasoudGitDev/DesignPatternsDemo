401ea1e6ae7c818eb7260fbe5b854ddfd95e5efc
MasoudSh <Msh.Git.Dev@gmail.com>
2024-04-29 22:49:06 +0330

    del : bad name

--- Behavioral/ChainOfResponsibility/Models/Food.cs
+++
@@ -1,4 +0,0 @@
-﻿using ChainOfResponsibility.Abstractions;
-
-namespace ChainOfResponsibility.Models;
-internal record Food(string Name , decimal Price) : IFood;
--- Behavioral/ChainOfResponsibility/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using ChainOfResponsibility;
-using ChainOfResponsibility.Models;
-
-Console.WriteLine("Hello, Chain Of Responsibility Pattern!\n");
-
-var food = new Food("Apple" , 100);
-Shop.Sell(food);
