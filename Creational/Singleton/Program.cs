bd8508be2e1586bd6e4dccdc4ba9111a0265d4e8
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-14 14:52:50 +0330

    update : write better description

--- Creational/Singleton/Program.cs
+++ Creational/Singleton/Program.cs
@@ -1,16 +1,19 @@
 ﻿using Singleton.Objects;
 
-Console.WriteLine("Hello, Singleton Pattern! \n");
+Console.WriteLine("Hello, Singleton Pattern!\n");
 
 Console.WriteLine("================== Compare HashCodes for Normal Objects\n");
 var normalClass1 = new NormalObject();
 var normalClass2 = new NormalObject();
-Console.WriteLine($"{normalClass1.GetHashCode()} != {normalClass2.GetHashCode()} => hash codes are different.\n");
-Console.WriteLine("Because every time that we use <new> keyword => this means =>  we try to create a new object!\n");
+Console.WriteLine($"{normalClass1.GetHashCode()} != {normalClass2.GetHashCode()} => Hash codes are different.\n");
+
+Console.WriteLine("Because every time we use the 'new' keyword, it creates a new object.\n");
 
 Console.WriteLine("================== Compare HashCodes for Singleton Objects\n");
 var singletonObj1 = SingletonObject.GetOrCreate();
 var singletonObj2 = SingletonObject.GetOrCreate();
-Console.WriteLine($"{singletonObj1.GetHashCode()} != {singletonObj2.GetHashCode()} => hash codes are equals.\n");
-Console.WriteLine("Because every time that we use GetOrCreate() method => this means => ");
-Console.WriteLine("if that object is available so get it else create new !");
+Console.WriteLine($"{singletonObj1.GetHashCode()} == {singletonObj2.GetHashCode()} => Hash codes are equal.\n");  
+
+Console.WriteLine("Because with the GetOrCreate() method, it checks if the object exists first.\n"); 
+Console.WriteLine("If the object exists, it returns it. Otherwise, it creates a new one.\n");
+
