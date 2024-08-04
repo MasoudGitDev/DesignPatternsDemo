2240f922874c636b7522a0ad286abf58fd93547b
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-04 12:27:23 +0330

    update : CustomExeption

--- Shared/Exceptions/CustomException.cs
+++ Shared/Exceptions/CustomException.cs
@@ -1,8 +1,8 @@
 ﻿namespace Shared.Exceptions;
 public class CustomException :Exception {
     public string Method { get; } = "<undefined>";
-    public string Code { get; } = "<undefined>";
-    public string Description { get; }
+    public string Code { get; private set; } = "<undefined>";
+    public string Description { get; private set; }
     public CustomException() : base() {
         Description = Message;
     }
@@ -20,6 +20,15 @@ public class CustomException :Exception {
         Method = method;
         Code = code;
         Description = description;
+    }
+
+    public void Update(string code) {
+        Code = code;
+    }
+
+    public void Update(string code , string message) {
+        Code = code;
+        Description = message;
     }
 
 
