4db385cf6185624bd5e6e4b393c4cde66d93561b
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-04 12:26:58 +0330

    update : add NextLine for console write line

--- Shared/Services/Logger.cs
+++ Shared/Services/Logger.cs
@@ -1,5 +1,6 @@
 ﻿namespace Shared.Services;
 public class Logger {
     public static void Log(string message)  => Console.Write(message + "\n");
+    public static void NextLine() => Console.WriteLine("\n");
     public static void CreateTitle(string message) => Console.WriteLine($"====================== {message} =================\n");
 }
